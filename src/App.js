05f0a43910c61cb83693fef95b7c3cd4cec3d2f8
suleman <suleman0708@hotmail.no>
2022-02-15 15:44:07 +0100

    routing with different pages

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import React from 'react';
 import Login from './components/Login';
 import peopleFeed from './components/peopleFeed';
 import eventFeed from './components/eventFeed';
+import SignUp from './components/signUp';
+import LandingPage from './components/landingPage';
+//import UserProfile from './components/profilePage';
+import Search from './components/searchPage';
+
 
 import './App.css';
+import './components/Login.css'
+import './components/landingpagephoto.css'
 
 function App() {
   return (
@@ -14,6 +21,11 @@ function App() {
       <Route path="/login" component={Login} />
       <Route path="/peoplefeed" component={peopleFeed} />
       <Route path="/eventfeed" component={eventFeed} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/landingpage" component={LandingPage} />
+     { /* <Route path="/users/:id" component={UserProfile} /> */}
+     <Route path="/search" component={Search} />
+
     </Switch>
   </HashRouter>
   );
