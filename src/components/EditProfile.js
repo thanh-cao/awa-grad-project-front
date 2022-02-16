ca3433c850ad2aa3274d039e6f39a5b68f7f93e2
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-02-16 15:47:46 +0100

    fix handle file input

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import placeholderImg from '../photos/placeholder-image.png';
 
 class EditProfile extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
@@ -14,14 +14,14 @@ class EditProfile extends React.Component {
             location: ''
         }
     }
-    componentDidMount(){
-        const {id} = this.props.match.params
+    componentDidMount() {
+        const { id } = this.props.match.params
 
         fetch(`${process.env.REACT_APP_API_URL}/users/${id}`)
-        .then(res => res.json())
-        .then(user => {
-            console.log(user);
-            this.setState({
+            .then(res => res.json())
+            .then(user => {
+                console.log(user);
+                this.setState({
                     isLoading: false,
                     profilePitcure: user.profilePicture,
                     about: user.about,
@@ -29,15 +29,15 @@ class EditProfile extends React.Component {
                     languages: user.languages,
                     location: user.locations
                 })
-        })
+            })
     }
 
-    handleSubmit(e){
-        const {id} = this.props.match.params
+    handleSubmit(e) {
+        const { id } = this.props.match.params
         e.preventDefault();
 
         const updatedUser = {
-            profilePictue: this.state.profilePicture,
+            profilePicture: this.state.profilePicture,
             about: this.state.about,
             interests: this.state.interests,
             languages: this.state.languages,
@@ -47,7 +47,7 @@ class EditProfile extends React.Component {
         fetch(`${process.env.REACT_APP_API_URL}/users/${id}`, {
             method: 'POST',
             credentials: 'include',
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(updatedUser)
@@ -55,48 +55,62 @@ class EditProfile extends React.Component {
     }
 
 
-    handleChange(e, field){
+    handleChange(e, field) {
         this.setState({
             [field]: e.target.value
         })
     }
 
-    render(){
-        const {isLoading, profilePicture, about, interests, languages, location} = this.state;
+    handleFileUpload(e) {
+        e.preventDefault();
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('profilePicture', file);
 
-        if(isLoading){
+        fetch(`${process.env.REACT_APP_API_URL}/services/imageupload`, {
+            method: 'POST',
+            credentials: 'include',
+            body: formData
+        }).then(response => response.json())
+            .then(result => this.setState({profilePicture: result}));
+    }
+
+    render() {
+        const { isLoading, profilePicture, about, interests, languages, location } = this.state;
+
+        if (isLoading) {
             return <div>Loading...</div>
         }
-        
-        return(
+
+        return (
             <div className="edit-profile">
-                <form 
-                encType="multipart/form-data" 
-                className="edit-form"
-                onSubmit={(e) => this.handleSubmit(e)}>
+                <form
+                    encType="multipart/form-data"
+                    className="edit-form"
+                    onSubmit={(e) => this.handleSubmit(e)}>
                     <div className="imageUpload">
-                        {profilePicture ? profilePicture : <img src={placeholderImg} /> }
-                        <input 
-                        type="file" 
-                        id="profilePicture" 
-                        name="profilePicture"
-                        onChange={(e) => this.handleChange(e, 'profilePicture')}
+                        {profilePicture ? profilePicture : <img src={placeholderImg} />}
+                        <input
+                            type="file"
+                            id="profilePicture"
+                            name="profilePicture"
+                            onChange={(e) => this.handleFileUpload(e)}
                         ></input>
                     </div>
                     <div>
                         <label>About me</label>
-                        <textarea 
-                        rows="4"
-                        value={about}
-                        onChange={(e) => this.handleChange(e, 'about')}
+                        <textarea
+                            rows="4"
+                            value={about}
+                            onChange={(e) => this.handleChange(e, 'about')}
                         ></textarea>
                     </div>
                     <div>
                         <label>Interests</label>
-                        <textarea 
-                        rows="4"
-                        value={interests}
-                        onChange={(e) => this.handleChange(e, 'interests')}
+                        <textarea
+                            rows="4"
+                            value={interests}
+                            onChange={(e) => this.handleChange(e, 'interests')}
                         ></textarea>
                     </div>
                     <div className="location">
@@ -106,9 +120,9 @@ class EditProfile extends React.Component {
                     </div>
                     <div>
                         <label>Language</label>
-                        <select 
-                        onChange={(e) => this.handleChange(e, 'language')}
-                        multiple>
+                        <select
+                            onChange={(e) => this.handleChange(e, 'language')}
+                            multiple>
                             <option>English</option>
                             <option>Norwegian</option>
                             <option>Swedish</option>
@@ -116,9 +130,9 @@ class EditProfile extends React.Component {
                             <option>German</option>
                         </select>
                     </div>
-                    <button 
-                    className="save-btn"
-                    type="submit">Save</button>
+                    <button
+                        className="save-btn"
+                        type="submit">Save</button>
                     <button className="cancel-btn">Cancel</button>
                 </form>
             </div>
