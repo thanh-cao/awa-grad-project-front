2597e235ca378c2ef885d21ba32b739497a4e7c7
suleman <suleman0708@hotmail.no>
2022-02-18 14:18:31 +0100

    search page with searchword

--- src/components/searchPage.js
+++ src/components/searchPage.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-const adress = 'Oslo'.replace(' ', '+')
-
-
-
-
-fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${adress},+CA&key=AIzaSyAGIPSSAJGsWmI8LPCFg5gqo4TZDRthXf8`)
-  .then(response => response.json())
-  .then(data => console.log(data.results[0].geometry.location));
-
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      textInput: ''
     };
+
+    this.textInput = React.createRef();
+    
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      //console.log("Latitude is :", position.coords.latitude);
-      //console.log("Longitude is :", position.coords.longitude);
-    });
+  handleChange(event) {
+    this.setState({
+      textInput: event.target.value
+    })
   }
 
+  handleSubmit(event, searchInput) {
+    event.preventDefault();
+
+    searchInput = this.state.textInput
+    return searchInput;
+  }
 
 
   render() {
@@ -35,7 +37,6 @@ class Search extends React.Component {
                 <input name='location' onChange={this.handleChange} type="text" placeholder='Search' id='searchButton'/>
             </label>
                 <button onClick={this.handleSubmit}>Search</button>
-            
             <div className='footer'></div>
       </div>
     );
