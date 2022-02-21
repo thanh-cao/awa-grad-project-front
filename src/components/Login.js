9f7b454874538a790888a53e68af4fc9103df3c8
Daniela Reijs <danielareijs@gmail.com>
2022-02-21 19:43:17 +0100

    simple layout styling for search

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Form} from 'react-bootstrap';
 
-const { loginUser, authenticateUser } = require('./loginUser');
+const { loginUser } = require('./loginUser');
 
 class Login extends React.Component {
   constructor(props) {
